ba28cf4e156fc180bf32823c5cb07938e7f30a06
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-04 22:50:37 +0200

    Update Randomized_min_cut.m

--- Randomized_min_cut.m
+++ Randomized_min_cut.m
@@ -1,54 +1,60 @@
-clear all;
-close all;
-clc
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% M file for testing the randomized min-cut algorithm             %
+%                                                                 %
+% A. P. Liavas, Feb. 25, 2015, March 1, 2021                      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear, clc
 
 % Number of nodes
-n = randi([50 100]);
-c=10;
-% Construction of random adjacency matrix
-A = zeros(n,n);
-for i=1:n
-    for j=i+1:n
-        A(i,j) = randi([1,10]);   % Experiment with this range
+n = 500;
+min_cut_weight = 100;
+
+A = generate_adjacency_matrix(n, min_cut_weight);
+
+for iter=1:10 % Repeat the whole process 
+    
+    iter 
+    
+    % Graph partitions - the algorithm returns two nodes 
+    opt1 = [];
+    opt2 = [];    
+    
+    inner_iter = 0;
+    found = 0;
+    while (found == 0)
+        
+        inner_iter = inner_iter + 1; 
+      
+        % Randomized min-cut algorithm 
+        % Initialization
+        AA = A;
+        nodes = cell(n,1);
+        for jj=1:n
+          nodes{jj} = jj;
+        end
+ 
+        % Main body
+        for ii=n:-1:3    % perform n-2 iterations  
+            [u, v] = find_edge_uniformly(AA);          % find edge (u,v) * uniformly at random * from existing edges
+            AA = update_adjacency_matrix(AA, u, v);    % contract edge 1) update adjacency matrix
+            nodes = update_nodes(nodes, u, v);         % contract edge 2) update (merge) nodes 
+        end
+      
+        
+        % Check weight of output 
+        if ( AA(1,2) == min_cut_weight )
+            found = 1;
+            fprintf('\nMin-cut found...\n');
+            opt1 = sort(nodes{1}(:));
+            opt2 = sort(nodes{2}(:));
+        else
+            fprintf('\nMin-cut failed...\n')
+        end
+   
     end
+    iters(iter) = inner_iter;
 end
 
-% Modify adjacency matrix to obtain fixed solution - try any other solution you want
-A(1,2:c+1) = 1;
-% Mirror the upper piece to the lower, which doubles the diagonal so subtract off the diagonal
-B = A + A.' - diag(diag(A)); 
-                             
-min_cut_sum = 10^6;   % Large initial value  
-% Graph partitions - the algorithm returns two nodes 
-opt1 = [];
-opt2 = [];    
-
-for kk=1:10 % n*(n-1)*log(n)   % Repeat randomized min-cut algorithm
-    kk
-
-    % Randomized min-cut algorithm 
-    % Initialization
-    AA = A;
-    nodes = cell(n,1);
-    for jj=1:n
-      nodes{jj} = jj;
-    end
-
-    % Main body
-    for ii=n:-1:3    % perform n-2 iterations  
-        [u, v] = find_edge_uniformly(AA);          % find edge (u,v) * uniformly at random * from existing edges
-        AA = update_adjacency_matrix(AA, u, v);    % contract edge 1) update adjacency matrix
-        nodes = update_nodes(nodes, u, v);         % contract edge 2) update (merge) nodes 
-    end
-
-    % Save best solution 
-    if ( AA(1,2) < min_cut_sum )
-       opt1 = nodes{1}(:)                       % component 1
-       opt2 = nodes{2}(:)                       % component 2
-       min_cut_sum = AA(1,2), pause             % size of cut-set
-    end
-end
-% 
-% opt1
-% opt2
-% min_cut_sum
+a = hist(iters, [1:max(iters)]);
+bar(a/iter)
