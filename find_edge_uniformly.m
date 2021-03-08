1536a923133e494adc5c3648545fdc4108ae4a14
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-08 21:12:40 +0200

    Create find_edge_uniformly.m

---
+++ find_edge_uniformly.m
@@ -0,0 +1,28 @@
+function [u, v] = find_edge_uniformly(A)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [u, v] = find_edge_uniformly(A)          %
+%                                          %
+% (u,v) edge selected uniformly at random  %
+% from the edges of adjacency matrix A     %
+%                                          %
+% A. P. Liavas, Febr. 26, 2015             %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[m, n] = size(A);
+
+num_of_edges = sum(sum(A))/2;          % total number of edges
+rand_edge = randi([1, num_of_edges]);  % random number in [1-num_of_edges]
+
+% Scan matrix A to identify the corresponding pair of nodes
+edge = 0;
+
+for ii=1:m
+    for jj=ii+1:n
+        edge = edge + A(ii,jj);
+        if (edge >= rand_edge)
+            u = ii;
+            v = jj;
+            return
+        end
+    end
+end
