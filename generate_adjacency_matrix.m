2b09e123182e2f43083d4399389145d386062c42
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-09 21:37:40 +0200

    Update generate_adjacency_matrix.m

--- generate_adjacency_matrix.m
+++ generate_adjacency_matrix.m
@@ -1,7 +1,5 @@
 function adjacency_matrix = generate_adjacency_matrix(n, min_cut_weight)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Number of nodes
-c=10;
 % Construction of random adjacency matrix
 A = zeros(n,n);
 for i=1:n
@@ -10,7 +8,8 @@ for i=1:n
     end
 end
 % Modify adjacency matrix to obtain fixed solution - try any other solution you want
-A(1,2:c+1) = 1;
+A(1,2:min_cut_weight+1) = 1;
+A(1,min_cut_weight+2:n) = 0;
 % Mirror the upper piece to the lower, which doubles the diagonal so subtract off the diagonal
-adjacency_matrix = A + A.' - diag(diag(A)); 
+adjacency_matrix = A + A';
 end
