d461233a7e9f65938ff2552e2326260e6d81d5a0
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-10 18:29:20 +0200

    Update update_nodes.m

--- update_nodes.m
+++ update_nodes.m
@@ -1,7 +1,5 @@
 function [ cell_out ] = update_nodes(cell_in, n1, n2)
 
-n = length(cell_in);
-
 cell_out = cell_in;
 cell_out{n1,:} = [cell_in{n1}; cell_in{n2}]; % merge super-nodes n1 and n2
 cell_out(n2,:) = [];                         % "kill" super-node n2
