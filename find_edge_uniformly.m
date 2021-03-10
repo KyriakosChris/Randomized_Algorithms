a1e78a099fa1571d28edc1c4e7e16b004adc6e6d
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-10 18:29:00 +0200

    Update find_edge_uniformly.m

--- find_edge_uniformly.m
+++ find_edge_uniformly.m
@@ -1,6 +1,6 @@
 function [u, v] = find_edge_uniformly(A)
 
-[m, n] = size(A);
+n = length(A);
 
 num_of_edges = sum(sum(A))/2;          % total number of edges
 rand_edge = randi([1, num_of_edges]);  % random number in [1-num_of_edges]
@@ -8,12 +8,12 @@ rand_edge = randi([1, num_of_edges]);  % random number in [1-num_of_edges]
 % Scan matrix A to identify the corresponding pair of nodes
 edge = 0;
 
-for ii=1:m
-    for jj=ii+1:n
-        edge = edge + A(ii,jj);
+for i=1:n
+    for j=i+1:n
+        edge = edge + A(i,j);
         if (edge >= rand_edge)
-            u = ii;
-            v = jj;
+            u = i;
+            v = j;
             return
         end
     end
