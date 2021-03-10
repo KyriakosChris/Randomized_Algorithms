aa993dd76380ec70d0f8d345f3deab022458b698
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-10 18:29:08 +0200

    Update update_adjacency_matrix.m

--- update_adjacency_matrix.m
+++ update_adjacency_matrix.m
@@ -1,11 +1,11 @@
 function [A_updated] = update_adjacency_matrix(A, i1, i2)
 
-[m, n] = size(A);
+n = length(A);
 
 A_updated = A;
 A_updated(i1, :) = A(i1,:) + A(i2,:);    % Add edges that are associated with nodes i1 and i2
 A_updated(:, i1) = A_updated(i1,:)';     %             symmetrize
-A_updated = A_updated([[1:i2-1] [i2+1:m]],[[1:i2-1] [i2+1:n]]);   % Contract node i2  
+A_updated = A_updated([1:i2-1 i2+1:n],[1:i2-1 i2+1:n]);   % Contract node i2  
 
 A_updated(i1, i1) = 0;                   % delete self-loops
 
