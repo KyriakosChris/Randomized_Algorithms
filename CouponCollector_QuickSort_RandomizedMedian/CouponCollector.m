cb1b4fb9187d8c7ad5eb8316a20899d31eb7c129
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-23 15:44:00 +0200

    Update CouponCollector.m

--- CouponCollector_QuickSort_RandomizedMedian/CouponCollector.m
+++ CouponCollector_QuickSort_RandomizedMedian/CouponCollector.m
@@ -2,30 +2,33 @@ clear all;
 close all;
 clc
 
-prompt_m = 'How many times would you like to run this?';
-prompt_coupon_num = 'How many coupons are in the set?';
-m = input(prompt_m); %input amount of repeats
-coupon_num = input(prompt_coupon_num); %input number of coupons
-x = zeros(1,coupon_num); %create a vector of all nums 1 -> couponnum
-for i = 1:coupon_num
-    x(i) = i;
-end
-s = sum(x);
-T = zeros(1,m); %create a vector of zeros which will track the steps till completion
+n =100; %input number of coupons
+m = n*log10(n); %input amount of repeats 
+x = 1:n;    %create a vector of all nums 1 -> couponnum
+T = zeros(1,ceil(m)); %create a vector of zeros which will track the steps till completion
+random = zeros(1,m);
 for l = 1:m 
     j = 0; %sets j = 0 at the start of each new repeat
-    y = zeros(1,coupon_num); %creates a vector of 0's for each new repeat
-    while j<s
-    r = randi([1,coupon_num]); %creates a random number between 1 and the max
-    for k = 1:coupon_num 
+    y = zeros(1,n); %creates a vector of 0's for each new repeat
+    while sum(x)~= sum(y)
+    r = randi([1,n]); %creates a random number between 1 and the max
+    random(l)= random(l)+r;
+    for k = 1:n 
         if r == y(k) %checks if the random number is already in the vector y
         else
             y(r) = r; %if not adds the number to the vector in the position of the number
+           
         end
     end
     j = sum(y); %tracks to see if all the coupons have been selected
     T(l) = T(l) + 1; %counts the number of times a selection has taken place
     end
 end
-T_mean = sum(T)/m; %calculates the mean
-disp(T_mean); 
+T_mean = sum(T)/length(T); %calculates the mean
+R_mean = sum(random)/length(random); %calculates the mean
+disp(T_mean)
+a = hist(T, [1:max(T)]);
+bar(a/m)
+title('Histogram')
+ylabel('Possibility')
+xlabel('Number of Iterations')
