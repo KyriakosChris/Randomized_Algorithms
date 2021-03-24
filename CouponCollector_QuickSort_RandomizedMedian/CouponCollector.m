d5ad3ca97d53aece0e102d3a0f2c8bfadb0e7ae5
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-24 18:01:07 +0200

    Updated

--- CouponCollector_QuickSort_RandomizedMedian/CouponCollector.m
+++ CouponCollector_QuickSort_RandomizedMedian/CouponCollector.m
@@ -8,27 +8,29 @@ x = 1:n;    %create a vector of all nums 1 -> couponnum
 T = zeros(1,ceil(m)); %create a vector of zeros which will track the steps till completion
 random = zeros(1,m);
 for l = 1:m 
+    disp(l)
     j = 0; %sets j = 0 at the start of each new repeat
     y = zeros(1,n); %creates a vector of 0's for each new repeat
     while sum(x)~= sum(y)
-    r = randi([1,n]); %creates a random number between 1 and the max
-    random(l)= random(l)+r;
-    for k = 1:n 
-        if r == y(k) %checks if the random number is already in the vector y
-        else
-            y(r) = r; %if not adds the number to the vector in the position of the number
-           
+        r = randi([1,n]); %creates a random number between 1 and the max
+        random(l)= random(l)+r;
+        for k = 1:n 
+            if r == y(k) %checks if the random number is already in the vector y
+            else
+                y(r) = r; %if not adds the number to the vector in the position of the number
+
+            end
         end
-    end
-    j = sum(y); %tracks to see if all the coupons have been selected
-    T(l) = T(l) + 1; %counts the number of times a selection has taken place
+        j = sum(y); %tracks to see if all the coupons have been selected
+        T(l) = T(l) + 1; %counts the number of times a selection has taken place
     end
 end
 T_mean = sum(T)/length(T); %calculates the mean
 R_mean = sum(random)/length(random); %calculates the mean
 disp(T_mean)
-a = hist(T, [1:max(T)]);
-bar(a/m)
+histogram(T)
+% a = hist(T, [1:max(T)]);
+% bar(a/m)
 title('Histogram')
 ylabel('Possibility')
-xlabel('Number of Iterations')
+xlabel('Number of Repetitions')
--- CouponCollector_QuickSort_RandomizedMedian/QuickSort.m
+++ CouponCollector_QuickSort_RandomizedMedian/QuickSort.m
@@ -1,3 +1,45 @@
 clear all;
 close all;
 clc
+% Quicksort implementation
+% This implementation works in place and runs in O(n)=n*log(n) (average case).
+% The purpose of this implementation is to show that quicksort can be
+% implemented in Matlab with O(n)=n*log(n) average case runtime.
+% This implementations, counts how many comparisons it needs until the
+% array is sorted.
+n=1000;
+global counter;
+simulations= 100;
+for l = 1:simulations
+    
+    A=randi(n,1,n); % create an array with n random numbers 
+    counter=0;
+    B=quicksort(A);
+    disp(counter);
+end
+function [y] = quicksort(x)
+    n=length(x);
+    global counter;
+    if n<2
+        y = x;
+        counter=counter+1;
+        return;
+    end
+
+    x1 = [];
+    x2 = [];
+
+    for i = 1:n-1
+
+        if x(i)<x(n)
+            x1 = [x1 x(i)];
+            counter=counter+1;
+        else
+            x2 = [x2 x(i)];
+            counter=counter+1;
+        end
+    end
+
+    y = [quicksort(x1) x(n) quicksort(x2)];
+
+end
