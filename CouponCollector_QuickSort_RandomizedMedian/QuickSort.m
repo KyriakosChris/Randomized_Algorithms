a3c157e3a215181a28ef775709433995e8f45124
KyriakosChris <23401502+KyriakosChris@users.noreply.github.com>
2021-03-26 12:27:01 +0200

    Update QuickSort.m

--- CouponCollector_QuickSort_RandomizedMedian/QuickSort.m
+++ CouponCollector_QuickSort_RandomizedMedian/QuickSort.m
@@ -1,29 +1,45 @@
 clear all;
 close all;
 clc
-n=1000;
-simulations= 100;
+simulations= 50;
+n = 200:200:10000;
 counterQuickSort=zeros(1,simulations);
 counterMedian=zeros(1,simulations);
-for l = 1:simulations
-    X = ['Simulation: ',num2str(l)];
+counterQuickSortHist=zeros(1,simulations);
+counterMedianHist=zeros(1,simulations);
+for i = 1:simulations
+    X = ['Simulation: ',num2str(i)];
     disp(X)
-    A=randi(n,1,n); % create an array with n random numbers 
-    [B,counterQuickSort(1,l)]=quicksortAndCount(A);
-    [m,counterMedian(1,l)] = RandomizedMedian(A);
+    A=randi(100000,1,n(i)); % create an array with n random numbers between 1 and 100000
+    C=randi(100000,1,1000); % create an array with n random numbers between 1 and 100000
+    % Creates Histogram, running in a 1000 element array for 50 simulations
+    [~,counterQuickSortHist(1,i)]=quicksortAndCount(C);
+    [~,counterMedianHist(1,i)] = RandomizedMedian(C);
+    % Creates Plot, running in a 200 to 10000 element array
+    [~,counterQuickSort(1,i)]=quicksortAndCount(A);
+    [~,counterMedian(1,i)] = RandomizedMedian(A);
 end
 CounterQuick_Mean = sum(counterQuickSort)/length(counterQuickSort); %calculates the mean
-X = ['Average Comparisons: ',num2str(CounterQuick_Mean)];
+X = ['Average QuickSort Comparisons: ',num2str(CounterQuick_Mean)];
 disp(X)
-histogram(counterQuickSort)
-title('Histogram')
+histogram(counterQuickSortHist)
+title('Histogram of QuickSort')
 ylabel('Possibility')
 xlabel('Number of Comparasons')
+
 figure
 CounterMedian_Mean = sum(counterMedian)/length(counterMedian); %calculates the mean
-X = ['Average Comparisons: ',num2str(CounterMedian_Mean)];
+X = ['Average Randomized Median Comparisons: ',num2str(CounterMedian_Mean)];
 disp(X)
-histogram(counterMedian)
-title('Histogram')
+histogram(counterMedianHist)
+title('Histogram of Randomized Median')
 ylabel('Possibility')
 xlabel('Number of Comparasons')
+
+figure
+plot(n,counterQuickSort);
+hold on;
+plot(n,counterMedian);
+legend('Counter of QuickSort', 'Counter of Randomized Median');
+hold off;
+suptitle('QuickSort And Randomized Median Comparasons')
